082b6598b55dc908956aa32118156c6811184bc7
Alessio <valessiobg@gmail.com>
2024-12-09 09:27:22 +0100

    creato qvc per 2 labels ma va sistemato

--- QVCpennylane2lables.py
+++ QVCpennylane2lables.py
@@ -1,32 +1,25 @@
-from sklearn import metrics
+from sklearn.model_selection import train_test_split
 import pennylane as qml
 from pennylane import numpy as np
 import numpy 
 from sklearn.preprocessing import normalize
 from math import floor
-from sklearn.model_selection import train_test_split
+
 
 shots=100
 dev = qml.device("default.qubit", shots=shots)
 
-data = numpy.genfromtxt("iris2.csv",delimiter=",", dtype=str)
+data = numpy.genfromtxt("iris3.csv",delimiter=",", dtype=str)
 X = np.array(data[:, :-1], dtype=float)
 #REMEMBER: devo indicare il tipo della label, cerco di essere il piu generico possibile in modo da classificare anche stringhe
-Y = numpy.array(data[:, -1])
+Y = numpy.array(data[:, -1], dtype=int)
 
 train_perc=0.75
 num_data = len(Y)
 dimensions = len(X[0])
 num_layers = 4
 batch_size = 20
-max_iterations=60
-num_steps_spsa =100
-
-#TODO: sto facendo un mix strano tra ausare la label come numero e come chiave, va rifatto
-#NOTE: set non è utilizzabile perchè non mantiene l'ordine delle label per qualche motivo
-classi= set(Y)
-
-print(classi)
+num_steps_spsa =300
 
 def ansatz(weights, iterations):
     for wire in range(dimensions):
@@ -64,7 +57,7 @@ def feature_map_layer(record):
     for wires in range(dimensions):
         phi=(record[wires])
         qml.PhaseShift(phi, wires=wires)
-    feature_map_ent("linear",record)
+    feature_map_ent("full",record)
 
 def feature_map_ent(ent, record):
     if ent=="circular":
@@ -81,6 +74,14 @@ def feature_map_ent(ent, record):
             qml.CNOT([wires, nxt])
             qml.PhaseShift(phi, wires=nxt)
             qml.CNOT([wires, nxt])
+    if ent=="full":
+        for target in range(1,dimensions):
+            for ctrl in range(target):
+                phi=(np.pi-record[ctrl])*(np.pi-record[target])
+                qml.CNOT([ctrl, target])
+                qml.PhaseShift(phi, wires=target)
+                qml.CNOT([ctrl, target])
+
 
 @qml.qnode(dev)
 def circuit(weights, x):
@@ -92,121 +93,77 @@ def circuit(weights, x):
     # for i in range(dimensions):
     #     qml.measure(i)
     #return qml.expval(qml.PauliZ(0))
-    return (qml.counts()) 
+    return (qml.counts(qml.Y(0))) 
 
 def print_circuit(weights,x):
     drawer = qml.draw(circuit)
     print(drawer(weights, x))
 
 def variational_classifier(weights, x, labels):
-    #matrice di grandezza 2^n x C, ovvero le possibili stringhe create dal circuito x il numero di classi
+#matrice di grandezza 2^n x C, ovvero le possibili stringhe create dal circuito x il numero di classi
 #utilizzo in realtà un array di dizionari perchè piu universale, cosi non sono limitato a numeri da 0 a C come etichette
-
     if len(x.shape)==1:
         results= circuit(weights, x)
-        #for string in results:
-        return mapper(results, labels)
+        return results
     else:
         raise TypeError("x must be a 1 dim list (a data element)")
 
-def mapper(q_strings,labels):
-    #varierà a seconda dei dati, qui in pratica sto usando la parità per decidere se un risultato è +1(se pari) o -1(dispari),
-    # questa parte è a piacere e non è molto rilevante
-    # la misurazione di pennylane da {0,1}, altri potrebbero dare {-1,1}
-    #metodi che useremo "Parity" (per binari, se pari 1 se dispari -1), "modulo" (per n label fai bitstring%n)
-    count={el:0 for el in labels}
-    n_per_classe=pow(2,dimensions)/len(labels)
-    for key, val in q_strings.items():
-        count[labels[floor(int(key,2)/n_per_classe)]]+=val
-    return count
-    
 #assumo data sia un multi dimensional dataset
 def normalize2pi(data):
     return normalize(data, norm="max",axis=0)*2*np.pi
 
-def square_loss_with_noise(labels, predictions):
-    L= np.mean((labels - np.sign(qml.math.stack(predictions))) ** 2)
-    noise = 5*np.random.random()
-    return L+noise
-
-def square_loss(labels, predictions):
-    # We use a call to qml.math.stack to allow subtracting the arrays directly
-    return np.mean((labels - np.sign(qml.math.stack(predictions))) ** 2)
-
-def softmax_max(x, beta=1.0):
-    weights = np.exp(beta * x)  # Calcolo dei pesi esponenziali
-    weighted_sum = np.sum(x * weights)  # Somma ponderata dei valori
-    weight_total = np.sum(weights)  # Somma dei pesi
-    return weighted_sum / weight_total
-
-def prob_x_div_corretto(pmf,correct):
+#FIXME: SISTEMA
+def prob_x_div_corretto(pmf,correct, bias):
     #dobbiamo capire quale è la probabilità che il predict di x sia diverso dalla sua corretta label e minimizzarlo
     py=pmf[correct]
-    rest=[x for i,x in enumerate(pmf) if i!=correct]
     #print(correct, pmf, py, rest)
-    f=np.sqrt(shots)*((max(rest)-py)/np.sqrt(2*(1-py)*py))
+    num=(1+correct*bias)/2-py
+    den=np.sqrt(2*(1-py)*py)
+    if den==0: print("errore")
+    f=np.sqrt(shots)*(num)/(den+1e-10)
     p=1/(1+np.exp(-f))
     return p
 
 
-def sig_cost_function(weights, X, Y):
+def sig_cost_function(weights, bias, X, Y):
     R_emp=0
     for x,y in zip(X,Y):
-        emp_distribution= [(i/shots) for i in variational_classifier(weights,x, classi).values()]
-        R_emp+=prob_x_div_corretto(emp_distribution,y)
+        samples=variational_classifier(weights,x, [-1,1])
+        emp_distribution= {k:v/shots for k,v in samples.items()}
+        R_emp+=prob_x_div_corretto(emp_distribution, y, bias)
         #print(prob_x_div_corretto(emp_distribution,y))
-        #print(R_emp)
-<<<<<<< HEAD
-    return R_emp/len(Y)
-=======
     return R_emp/len(X)
->>>>>>> 9042b22 (creato qvc per 2 labels ma va sistemato)
 
-def old_cost(weights, bias, X, Y):
-    predictions=[variational_classifier(weights, bias, x)for x in X]
-    #print(qml.math.stack(predictions))
-    #puoi scegliere quale loss function utilizzare
-    return square_loss(Y, predictions)
-
-#è sbagliata??
-def cross_entropy_cost(weights, X, Y):
-    R_emp=0
-    for x,y in zip(X,Y):
-        emp_distribution= [(i/shots) for i in variational_classifier(weights,x, classi).values()]
-        R_emp-=np.log(emp_distribution[y])
-    #loss
-<<<<<<< HEAD
-    return R_emp/len(Y)
-=======
-    return R_emp/len(X)
->>>>>>> 9042b22 (creato qvc per 2 labels ma va sistemato)
-
+#FIXME: non ha senso?
 def accuracy(labels, predictions):
     acc = sum(abs(l - p) < 1e-5 for l, p in zip(labels, predictions))
     acc = acc / len(labels)
     return acc
 
-
 def run_optimizer(opt, cost_function, init_param, num_steps, interval, execs_per_step):
     # Copy the initial parameters to make sure they are never overwritten
-    weights,X,Y = init_param.copy()
+    weights, bias, X, Y = init_param.copy()
 
     # Initialize the memory for cost values during the optimization
     cost_history = []
     # Monitor the initial cost value
-    cost_history.append(cost_function(weights,X,Y))
+    cost=cost_function(weights,bias, X,Y)
+    cost_history.append(cost)
     exec_history = [0]
-
+    f=open('dump.txt', 'w')
     print(
         f"\nRunning the {opt.__class__.__name__} optimizer for {num_steps} iterations."
     )
-    for step in range(num_steps):
+    step=0
+    for i in range(num_steps):
         # Print out the status of the optimization
         if step % 10 == 0:
             print(
                 f"Step {step:3d}: Circuit executions: {exec_history[step]:4d}, "
                 f"Cost = {cost_history[step]}"
             )
+            f.write(f"Step {step:3d}: Circuit executions: {exec_history[step]:4d}, "
+                f"Cost = {cost_history[step]}")
 
         # batch_index = np.random.randint(0, len(X), (batch_size,))
         # feats_train_batch = X[batch_index]
@@ -214,92 +171,29 @@ def run_optimizer(opt, cost_function, init_param, num_steps, interval, execs_per
         # Perform an update step
         # weights, _, _ = opt.step(cost_function, weights, feats_train_batch, Y_train_batch)
 
-        weights, _, _ = opt.step(cost_function, weights, X, Y)
+        weights, bias, _, _ = opt.step(cost_function, weights, bias, X, Y)
         #print(weights)
         # Monitor the cost value
-
-        cost_history.append(cost_function(weights,X,Y))
+        cost=cost_function(weights, bias, X,Y)
+        cost_history.append(cost)
         exec_history.append((step + 1) * execs_per_step)
+        step+=1
 
     print(
         f"Step {num_steps:3d}: Circuit executions: {exec_history[-1]:4d}, "
         f"Cost = {cost_history[-1]}"
     )
+    f.write(f"Step {num_steps:3d}: Circuit executions: {exec_history[-1]:4d}, "
+        f"Cost = {cost_history[-1]}")
     return cost_history, exec_history, weights
 
-<<<<<<< HEAD
-=======
-def grad(L, w, ck):
-     
-    # number of parameters
-    p = len(w)
-     
-    # bernoulli-like distribution
-    deltak = np.random.choice([-1, 1], size=p)
-     
-    # simultaneous perturbations
-    ck_deltak = ck * deltak
- 
-    # gradient approximation
-    up=L(w + ck_deltak)
-    down=L(w - ck_deltak)
-    DELTA_L = up - down 
-    print("-------------------------")
-    print(up)
-    print(down)
-    return (DELTA_L) / (2 * ck_deltak)
 
-def initialize_hyperparameters(alpha, lossFunction, w0, N_iterations):
- 
-    c = 0.5  # a small number
- 
-    # A is <= 10% of the number of iterations
-    A = N_iterations*0.1
-    
-    # order of magnitude of first gradients
-    magnitude_g0 = np.abs(grad(lossFunction, w0, c).mean())
-    print(magnitude_g0)
-    # the number 2 in the front is an estimative of
-    # the initial changes of the parameters,
-    # different changes might need other choices
-    a = 2*((A+1)**alpha)/(magnitude_g0+1e-10)
- 
-    return a, A, c
-
-def SPSA(LossFunction, parameters, alpha=0.602, gamma=0.101, N_iterations=10):
-     
-    # model's parameters
-    w = parameters
-    a, A, c = initialize_hyperparameters(
-      alpha, LossFunction, w, N_iterations)
-    for k in range(1, N_iterations):
- 
-        # update ak and ck
-        ak = a/((k+A)**(alpha))
-        ck = c/(k**(gamma))
-        # estimate gradient
-        gk = grad(LossFunction, w, ck)
-        # update parameters
-        w -= ak*gk
- 
-    return w
-
-def confusion_matrix(x, y, labels):
-    pred=numpy.empty(len(x),dtype=str)
-    for i in range(len(x)):
-        emp=variational_classifier(weights,x[i],labels)
-        l=max(zip(emp.values(), emp.keys()))[1]
-        pred[i]=l
-    confusion_matrix = metrics.confusion_matrix(y, pred)
-    return confusion_matrix
-
->>>>>>> 9042b22 (creato qvc per 2 labels ma va sistemato)
 ################### main ###################
 
 print("prima riga pre normalizzazione",X[0],"   ",Y[0])
 features=normalize2pi(X)
 print("prima riga post normalizzazione", features[0],"   ", Y[0])
-if len(Y)==len(features): print("corrette dimesioni")
+if num_data==len(features): print("corrette dimesioni")
 
 #suddivisione 
 
@@ -328,26 +222,26 @@ print(feats_train)
 #come descritto nel paper, limito le rotazioni su Pauli Z e Y,
 #uso una lista anziche una matrice
 #TODO: probabilmente i pesi iniziali sono un problema
-weights_init = np.random.random_sample((num_layers+1)*dimensions*2,requires_grad=True)-0.5 # forse non serve 
+weights_init = np.random.random_sample((num_layers+1)*dimensions*2,requires_grad=True) # forse non serve 
 #solo per dati binari
-#bias_init = np.array(0.0, requires_grad=True)
+bias_init = np.array(0.0, requires_grad=True)
 
 print("Weights:", weights_init)
-#print("Bias: ", bias_init)
+print("Bias: ", bias_init)
 
 weights = weights_init
-#bias = bias_init
+bias = bias_init
 
 
 ############# PRINT PER CONTROLLARE IL CIRCUITO #######################
 print(feats_train[0])
 print_circuit(weights, feats_train[0])
 
-input("Press Enter to continue...")
+#input("Press Enter to continue...")
 
 ######################## ZONA DI OBLIO DOVE TUTTO VIENE MODIFICATO ###########################
 
-opt= qml.GradientDescentOptimizer(num_steps_spsa,)
+opt= qml.SPSAOptimizer(num_steps_spsa,)
 
 # test=[0.294, 0.564, 0.142]
 # py=test[1]
@@ -358,37 +252,25 @@ opt= qml.GradientDescentOptimizer(num_steps_spsa,)
 # p=1/(1+np.exp(-f))
 # print(p)
 
-<<<<<<< HEAD
-pred=numpy.empty(len(feats_train))
+pred=numpy.empty(len(feats_train),dtype=str)
 for i in range(len(feats_train)):
-    emp=variational_classifier(weights,feats_train[i],classi)
-    print(emp)
+    emp=variational_classifier(weights,feats_train[i],[-1,1])
     l=max(zip(emp.values(), emp.keys()))[1]
     pred[i]=l
-confusion_matrix = metrics.confusion_matrix(Y_train, pred)
-print(confusion_matrix)
-=======
-confusion_matrix(feats_train,Y_train,classi)
->>>>>>> 9042b22 (creato qvc per 2 labels ma va sistemato)
+print(accuracy(Y_train,feats_train))
 
 cost_history_spsa, exec_history_spsa, weights = run_optimizer(
-opt, cross_entropy_cost, [weights,feats_train,Y_train], num_steps_spsa, 20, 1
+opt, sig_cost_function, [weights,bias,feats_train,Y_train], num_steps_spsa, 20, 1
 )
 
-# weights = SPSA(LossFunction = lambda parameters: cost_function(parameters, feats_train, Y_train),
-#                   parameters = weights)
-
-<<<<<<< HEAD
-pred=numpy.empty(len(feats_train))
+pred=numpy.empty(len(feats_train),dtype=str)
 for i in range(len(feats_train)):
-    emp=variational_classifier(weights,feats_train[i],classi)
-    print(emp)
-    pred[i]=int(max(zip(emp.values(), emp.keys()))[1])
-print(accuracy(Y_train,pred))
-=======
-confusion_matrix(feats_train,Y_train,classi)
-
->>>>>>> 9042b22 (creato qvc per 2 labels ma va sistemato)
+    emp=variational_classifier(weights,feats_train[i],[-1,1])
+    l=max(zip(emp.values(), emp.keys()))[1]
+    pred[i]=l
+print(accuracy(Y_train,feats_train))
+# weights = SPSA(LossFunction = lambda parameters: cross_entropy_cost(parameters, feats_train, Y_train),
+#                    parameters = weights)
 
 # print(weights)
 
